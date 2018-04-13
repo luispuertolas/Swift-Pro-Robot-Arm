a566a6ebe84b2c14ae2de0277c9df60ce4aeb658
luispuertolas <lapb@protonmail.com>
2018-04-13 21:48:04 +0100

    Update Swift PC.py

--- Swift PC.py
+++ Swift PC.py
@@ -36,24 +36,45 @@ swift.set_position(150, 0, 150, speed = 5000, timeout = 20)
 swift.flush_cmd()
 # wait all async cmd return before send sync cmd
 y=0
-x = 1
+x=150
 while True:
-    x += 1
     line = ser.readline()
     try:
         line = line.decode()
-        for x in range(len(line)):
-            if line[x] == ",":
-                xpos = line[:x]
-                ypos = line[x+2:len(line)]
+        for w in range(len(line)):
+            if line[w] == ",":
+                xpos = line[:w]
+                ypos = line[w+2:len(line)]
                 print(xpos,",", ypos)
                 if int(xpos) < 159:
                     y = y - 1
                     swift.set_position(150, int(y), 150, speed = 5000)       
                 if int(xpos) > 161:
                     y = y + 1
-                    swift.set_position(150, int(y), 150, speed = 5000)
+                    swift.set_position(150, int(y), 150, speed = 5000)                
                 if int(xpos) > 159 and int(xpos) < 161:
-                    swift.set_position(150, int(y), -112, speed = 5000)
+                    exitloop=0
+                    while exitloop == 0:
+                            line = ser.readline()
+                            try:
+                                line = line.decode()
+                                for w in range(len(line)):
+                                    if line[w] == ",":
+                                        xpos = line[:w]
+                                        ypos = line[w+2:len(line)]
+                                        print(xpos,",", ypos)
+                                        if int(ypos) < 119:
+                                            x = x - 1
+                                            swift.set_position(int(x), int(y), 150, speed = 5000)       
+                                        if int(ypos) > 121:
+                                            x = x + 1
+                                            swift.set_position(int(x), int(y), 150, speed = 5000)                            
+                                        if int(ypos) > 119 and int(ypos) < 121:
+                                            swift.set_position(int(x), int(y), -112, speed = 5000)
+                                            exitloop = 1
+                            except:
+                                x=x                    
     except:
         x=x
+
+
